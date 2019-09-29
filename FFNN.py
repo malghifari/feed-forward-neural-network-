75c0947eaf1dcef5821a8ee293c032aa1e9dab26
raflyalk <raflyalk@gmail.com>
2019-09-29 16:03:25 +0700

    masih error, alghi mau benerin

--- FFNN.py
+++ FFNN.py
@@ -1,5 +1,7 @@
 import numpy as np
 from Layer import Layer
+# from Layer import Layer
+
 
 class FFNN:
     def __init__(self, batch_size=1, n_hidden_layers=1, nb_nodes=2, learning_rate=0.1, momentum=0.1, epoch=1):
@@ -17,31 +19,29 @@ class FFNN:
         # Create list of layers (while initiating random weights)
         self.layer_list = []
         self.layer_list.append(Layer(n_neuron=self.nb_nodes, n_input=n_features))
-        self.layer_list = [Layer(n_neuron=self.nb_nodes, n_input=self.nb_nodes) for i in range(self.n_hidden_layers - 1)]
-        self.layer_list.append(Layer(n_neuron=1, n_input=n_features))
+        self.layer_list += [Layer(n_neuron=self.nb_nodes, n_input=self.nb_nodes) for i in range(self.n_hidden_layers - 1)]
+        self.layer_list.append(Layer(n_neuron=1, n_input=self.nb_nodes))
 
-        pred = self.predict(X)
         i = 0
         while (i < n_input):
+            for j in range(self.batch_size):
+                pred = self.predict(X[i+j])
+                label = y[i+j]
 
-            label = y[i]
-
-            for index, layer in enumerate(reversed(self.layer_list)):
-                if (index == 0):
-                    # delta for output layer
-                    delta = layer.compute_delta_output_layer(y)
-                    
-                else:
-                    # delta for hidden layer
-                    layer.gradient_descent(delta)
-                    delta = layer.compute_delta(delta)
+                for index, layer in enumerate(reversed(self.layer_list)):
+                    if (index == 0):
+                        # delta for output layer
+                        delta = layer.compute_delta_output_layer(y)
+                        layer.gradient_descent(delta)
+                    else:
+                        # delta for hidden layer
+                        delta = layer.compute_delta(delta)
+                        layer.gradient_descent(delta)
+    
+            for layer in self.layer_list:
+                layer.update_weight(n_input)
             
-            i += 1
-
-        for layer in self.layer_list:
-            layer.update_weight(n_input)
-
-            
+            i += self.batch_size
 
     def predict(self, input):
         output = input
@@ -49,38 +49,84 @@ class FFNN:
             output = layer.feed_forward(output)
         # return 1 if output[0] > 0.5 else 0
         return output[0]
-    
-    def cost(self, X, y):
-        m = X.shape[0];
-        return 1 / (2 * m) * sum(([self.predict(X[i]) for i in range(m)] - y) ** 2)
-
 
 import pandas as pd
-from scipy.io import arff
 
-data = arff.loadarff('dataset/weather.arff')
-
-df = pd.DataFrame(data[0])
+df = pd.read_csv('dataset/Churn_Modelling.csv')
 
 df.head()
 
+features = ['CreditScore','Age','Tenure','Balance','NumOfProducts','HasCrCard','IsActiveMember','EstimatedSalary']
 
-df['outlook'] = pd.Categorical(df['outlook']).codes
-df['windy'] = pd.Categorical(df['windy']).codes
-df['play'] = pd.Categorical(df['play']).codes
+preproc_df = df[features]
 
-df.head()
+preproc_df['Geography'] = pd.Categorical(df['Geography']).codes
+# preproc_df['Gender'] = pd.Categorical(df['Gender']).codes
+
+preproc_df.head()
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+
+scaled_df = scaler.fit_transform(preproc_df.to_numpy())
+
+label = df['Exited'].to_numpy()
+
+label
+
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import StratifiedShuffleSplit
+
+sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=0)
+X = scaled_df
+y = label
+for train_index, test_index in sss.split(X, y):
+    training_input, testing_input = X[train_index], X[test_index]
+    training_label, testing_label = y[train_index], y[test_index]
 
 
-feature = ['outlook', 'temperature', 'humidity', 'windy']
-X = df[feature].to_numpy()
-y = df['play'].to_numpy()
+from FFNN import FFNN
+
+ffnn = FFNN(batch_size=5, n_hidden_layers=2, nb_nodes=4, learning_rate=0.1, momentum=0.9, epoch=1)
+
+ffnn.fit(training_input, training_label)
+
+ffnn_pred = ffnn.predict(testing_input)
+
+print(classification_report(testing_label, ffnn_pred))
+
+
+# import pandas as pd
+# from scipy.io import arff
+
+# data = arff.loadarff('dataset/weather.arff')
+
+# df = pd.DataFrame(data[0])
+
+# df.head()
+
+
+# df['outlook'] = pd.Categorical(df['outlook']).codes
+# df['windy'] = pd.Categorical(df['windy']).codes
+# df['play'] = pd.Categorical(df['play']).codes
+
+# df.head()
+
+
+# feature = ['outlook', 'temperature', 'humidity', 'windy']
+# X = df[feature].to_numpy()
+# y = df['play'].to_numpy()
 
 
 
 
-ffnn = FFNN()
+# ffnn = FFNN()
 
-ffnn.fit(X, y)
+# ffnn.fit(X, y)
 
-print(ffnn.predict([1, 70.0, 96.0, 0]))
+# print(ffnn.predict([1, 70.0, 96.0, 0]))
+
+# from sklearn.neural_network import MLPClassifier
+
+# mlp = MLPClassifier(learning_rate_init=0.1, , solver='sgd', batch_size=10, momentum=0.9)
--- Layer.py
+++ Layer.py
@@ -12,7 +12,7 @@ class Layer:
             self.random_weight()
         else:
             self.weights = weights
-        self.Delta = np.full((self.n_neuron, self.n_input), 0)
+        self.Delta = np.full((self.n_neuron, self.n_input), 0, dtype='float64')
 
     def random_weight(self):
         self.weights = 2 * self.random_scale * \
@@ -24,22 +24,29 @@ class Layer:
         return 1 / (1 + math.exp(-z))
 
     def sigmoid_derivative(self, z):
-        return np.dot(self.sigmoid(z), (1 - self.sigmoid(z)))
+        return self.sigmoid(z) * (1 - self.sigmoid(z))
 
     def feed_forward(self, input):
         sigmoid_func = np.vectorize(self.sigmoid)
+        print(input.shape, self.weights.T.shape)
+        
         self.z = np.matmul(input, self.weights.T)
         self.a = sigmoid_func(self.z)
         return self.a
 
     def gradient_descent(self, previous_delta):
-        self.Delta += np.dot(self.a, previous_delta)
+        print(self.a.shape, previous_delta.shape)
+        print('Delta', self.Delta.shape)
+        self.Delta += self.a * previous_delta
 
     def compute_delta_output_layer(self, label):
-        return self.a - label
+        print(self.a, label)
+        print('aa', (self.a.T -label).shape)
+        return (self.a.T - label).T
 
     def compute_delta(self, previous_delta):
-        return np.dot(np.matmul(self.weights.T, previous_delta), self.sigmoid_derivative(self.z))
+        print(self.weights.T.shape, previous_delta.shape, self.sigmoid_derivative(self.z).shape)
+        return np.matmul(self.weights.T, previous_delta) * self.sigmoid_derivative(self.z)
 
     def update_weight(self, n_input):
         self.weights -= self.Delta / n_input
